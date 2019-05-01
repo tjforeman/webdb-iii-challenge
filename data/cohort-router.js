b3d21284c76ca06e80ae8b4689b82dd61c44aeaf
tjforeman <tylerforeman92@gmail.com>
2019-05-01 16:46:11 -0400

    able to get by id, post, put, and delete on cohort

--- data/cohort-router.js
+++ data/cohort-router.js
@@ -10,6 +10,21 @@ useNullAsDefault:true,
 }
 
  const db =knex(knexConfig);
+ 
+ router.post('/', (req, res) => {
+    if (!req.body.name){
+        res.status(400).json({message:'A name is required to add a Cohort'})
+    }else{
+    db('cohorts')
+    .insert(req.body,'id')
+    .then(cohort =>{
+      res.status(200).json(cohort)
+    })
+    .catch(err=>{
+      res.status(500).json(err)
+    })
+}
+  });
 
  router.get('/', (req, res) => {
     db('cohorts')
@@ -17,6 +32,58 @@ useNullAsDefault:true,
       res.status(200).json(cohorts)
     }).catch(err =>{
       console.log(err)
+    })
+  });
+
+  router.get('/:id', (req, res) => {
+    db('cohorts')
+    .where({id:req.params.id})
+    .first()
+    .then(cohort=>{
+      if(cohort){
+      res.status(200).json(cohort)
+      }else{
+        res.status(404).json({message:'the specified Cohort does not exist'})
+      }
+    })
+    .catch(err =>{
+      res.status(500).json(err)
+    })
+  });
+
+   router.put('/:id', (req, res) => {
+    if (!req.body.name){
+        res.status(400).json({message:'A name is required to edit a Cohort'})
+    }else{
+    db('cohorts')
+    .where({id:req.params.id})
+    .update(req.body)
+    .then(count=>{
+      if (count>0) {
+        res.status(200).json({message:`${count} record was updated`})
+      }else{
+        res.status(404).json({message:'the specified Cohort does not exist'})
+      }
+    })
+    .catch(err =>{
+      res.status(500).json(err)
+    })
+}
+  });
+
+   router.delete('/:id', (req, res) => {
+    db('cohorts')
+    .where({id:req.params.id})
+    .del()
+    .then(count =>{
+      if (count>0){
+        res.status(200).json({message:`${count} Cohort was deleted`})
+      }else{
+        res.status(400).json({message:'the specified Cohort does not exist'})
+      }
+    })
+    .catch(err =>{
+      res.status(500).json(err)
     })
   });
 
